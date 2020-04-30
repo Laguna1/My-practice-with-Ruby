335338091ac4511df74ad835bed790729c18d096
Laguna1 <lagunazoomarket@gmail.com>
2020-05-01 02:27:53 +0300

    array

--- array.rb
+++ array.rb
@@ -1,14 +1,44 @@
-new_array = (0..10).to_a   
-puts new_array
+# # new_array = (0..10).to_a   
+# # puts new_array
 
-array = ["a", "b", "c", "d", "e"]
-puts array[array.size - 2]               
-puts array[-2]
+# # array = ["a", "b", "c", "d", "e"]
+# # puts array[array.size - 2]               
+# # puts array[-2]
 
-array = [1, 2, 3, 4, 5, 5, 6, 0, 1, 2, 3, 4, 5, 7]
-puts array.uniq
-puts array.size
-puts array.length
+# # array = [1, 2, 3, 4, 5, 5, 6, 0, 1, 2, 3, 4, 5, 7]
+# # puts array.uniq
+# # puts array.size
+# # puts array.length
 
-array = [1, nil, 2, nil, 3]
-puts array.compact
+# # array = [1, nil, 2, nil, 3]
+# # puts array.compact
+
+#  fruits = ["apple", "lemon", "banana", "pineapple", "melon"]
+#  puts fruits.to_s
+
+#  new_fruits = []
+#  new_fruits << "grapefruit"
+#  new_fruits.push("cherry", "mango")
+#  puts "basket: " + new_fruits.to_s
+# # puts new_fruits[2]
+#  # new_fruits.delete("cherry")
+#  new_fruits.delete_at(0)
+#  puts new_fruits.to_s
+
+#  men = ["Gena", "George", "Misha"]
+
+#  women = ["Katrin", "Liza", "Masha"]
+# sum = men + women
+# puts sum.to_s
+# arr = [1, 2, 3, 4, 5]
+# puts arr.to_s
+# puts arr.reverse.to_s
+# puts arr.to_s
+# arr.reverse
+# puts arr.reverse!.to_s
+cars = ["Acura", "Audi", "Bentley", "Mersedes", "Sitroen", "Pegout", "Renault", "Ford", "Fiat", "Kia"]
+puts cars.length
+
+choice = gets.chomp
+
+puts choise
--- guess-the-number.rb
+++ guess-the-number.rb
@@ -2,7 +2,7 @@ puts "Try to guess the number from 0 to 15 in three attempts."
 
     computer_choice = rand(16)
 # puts computer_choice
-
+sleep 2
 puts "Ok, your first attempt..."
    user_choice = gets.chomp.to_i
 # puts user_choice
@@ -14,9 +14,9 @@ else
     if computer_choice > user_choice
         puts "Your number is less, try again."
     else
-        puts "Your number is greater, try again."
+        puts "Your number is too big, try again."
     end
-
+sleep 2
     if (computer_choice - user_choice).abs > 3
         puts "Far, try more precisely!"
         
@@ -24,7 +24,7 @@ else
         puts "You were almost there!"
     end
 end
-
+sleep 2
 puts "Ok, your second attempt..."
    user_choice = gets.chomp.to_i
 
@@ -37,9 +37,9 @@ else
     if computer_choice > user_choice
         puts "Your number is less, try again."
     else
-        puts "Your number is greater, try again."
+        puts "Your number is too bog, try again."
     end
-
+sleep 2
     if (computer_choice - user_choice).abs > 3
         puts "Far, try more precisely!"
         
@@ -47,6 +47,7 @@ else
         puts "You were almost there!"
     end
 end
+sleep 2
 puts "Ok, your third attempt..."
    user_choice = gets.chomp.to_i
 
@@ -59,7 +60,7 @@ else
     if computer_choice > user_choice
         puts "Your number is less, oops."
     else
-        puts "Your number is greater, oops."
+        puts "Your number is too big, oops."
     end
 
     if (computer_choice - user_choice).abs > 3
@@ -69,7 +70,8 @@ else
         puts "You were almost there!"
     end
         
-
+sleep 2
 puts "Try to win next time!"
-puts "This time you're out of luck. A number was ordered " + computer_choice.to_s + " ."
+sleep 2
+puts "This time you're out of luck. A number was ordered " + computer_choice.to_s + "!"
 end
