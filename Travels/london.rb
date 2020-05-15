c65ad3e22b22c0958f90b23c43962a2d235cc724
Laguna1 <lagunazoomarket@gmail.com>
2020-05-15 22:36:34 +0300

    added Guess-word and Visit-games

---
+++ Travels/london.rb
@@ -0,0 +1,4 @@
+def visit_london
+puts "Arrived in London"
+puts "Saw - Big Ben, Trafalgar Square, Buckingham Palace"
+end
---
+++ Travels/roma.rb
@@ -0,0 +1,4 @@
+def visit_roma
+puts "Arrived in Rome"
+puts "Saw - Pantheon, St. Peter's Basilica, Colosseum"
+end
