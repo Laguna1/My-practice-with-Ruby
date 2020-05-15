3668d64c4bca8ba65615bc0f383e4c6debd25084
Laguna1 <lagunazoomarket@gmail.com>
2020-05-15 23:03:47 +0300

    class in Ruby

---
+++ Classes, Objects/bridge.rb
@@ -0,0 +1,11 @@
+class Bridge
+    def initialize
+      puts "The bridge created just now"
+    end
+
+    def open_bridge
+    puts "The bridge is open, you can go!"
+    end
+
+
+end
---
+++ Classes, Objects/road.rb
@@ -0,0 +1,19 @@
+require_relative "./bridge.rb"
+puts "Let`s go!"
+sleep 1
+
+puts "Let's go"
+sleep 1
+
+puts "Oh, we have to cross the river!"
+sleep 1
+
+puts "No problem"
+sleep 1
+
+bridge = Bridge.new
+sleep 1
+bridge.open_bridge
+sleep 1
+
+puts "Good luck!"
