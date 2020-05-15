c749b1eb3173dc4b3843c632048deb388a606a99
Laguna1 <lagunazoomarket@gmail.com>
2020-05-15 22:33:42 +0300

    added TTT-game

--- guess-the-number-def.rb
+++ guess-the-number-def.rb
@@ -1,51 +1,57 @@
-#"Guess the number" game using the method
+# #"Guess the number" game using the method
 
 
-def check_number(computer_choice, user_choice)
+# def check_number(computer_choice, user_choice)
 
-    if computer_choice == user_choice
-    puts "You are a really cool dude!"
+#     if computer_choice == user_choice
+#     puts "You are a really cool dude!"
 
-       exit
-     end
+#        exit
+#      end
   
 
-    if computer_choice > user_choice
-        puts "Your number is less, try again."
-   else
-        puts "Your number is too big, try again."
-    end
+#     if computer_choice > user_choice
+#         puts "Your number is less, try again."
+#    else
+#         puts "Your number is too big, try again."
+#     end
   
    
-    if (computer_choice - user_choice).abs < 3
-        puts "You were almost there!"
-    else
-        puts "Far, try more precisely!"
-    end
-  end
+#     if (computer_choice - user_choice).abs < 3
+#         puts "You were almost there!"
+#     else
+#         puts "Far, try more precisely!"
+#     end
+#   end
 
-computer_choice = rand(16)
+# computer_choice = rand(16)
 
-puts "Try to guess the number from 0 to 15 in three attempts."
-sleep 2
+# puts "Try to guess the number from 0 to 15 in three attempts."
+# sleep 2
 
-puts "Ok, your attempt..."
-user_choice = gets.to_i
-check_number(computer_choice, user_choice)
+# puts "Ok, your attempt..."
+# user_choice = gets.to_i
+# check_number(computer_choice, user_choice)
 
-user_choice = gets.to_i
-check_number(computer_choice, user_choice)
+# user_choice = gets.to_i
+# check_number(computer_choice, user_choice)
 
-user_choice = gets.to_i
-check_number(computer_choice, user_choice)
-
-
-
-sleep 2
-puts "Try to win next time!"
-sleep 2
-puts "This time you're out of luck. A number was ordered " + computer_choice.to_s + "!"
+# user_choice = gets.to_i
+# check_number(computer_choice, user_choice)
 
 
 
+# sleep 2
+# puts "Try to win next time!"
+# sleep 2
+# puts "This time you're out of luck. A number was ordered " + computer_choice.to_s + "!"
+
+
+arr = [4, 3, 2, "hello"]
+def end_arr_delete(arr)
+    last = arr.pop(arr.length - 1)
+    return last
+    # delete the element from the end of the array and return the deleted element
+    
+end
   
--- methods.rb
+++ methods.rb
@@ -8,3 +8,33 @@ puts array.pop.to_s
 puts array[1]
 array.pop(2)
 puts array.to_s
+
+# array = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# def even_num(array)
+# new_ array = []
+# array.each { |item|item % 2 == 0 }
+# puts new_array.to_s
+
+
+# array = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# def delete_arr(array)
+#     arr.delete_if { |a| a % 2 != 0 }
+#   # delete all negative elements
+#   return array
+# end
+
+# puts array.filter.to_sa = ["a", "b", "c"]
+array = [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+i = 0
+sum = 0
+n = 10
+while array[i] <= n do
+    array.delete_if {|x| x % 2 != 0 }  
+
+end
+
+    array.each { |x| sum += x }
+    i += 1
+
+ 
+puts sum.to_s                             
